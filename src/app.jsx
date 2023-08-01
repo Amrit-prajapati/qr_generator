ebdc7690dbe47964081699717e99a18d2c8bd77a
Amrit <83388717+Amrit-prajapati@users.noreply.github.com>
2023-08-01 12:49:53 +0000

    Update index.css and app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 function App (){
 
-    const [txt , setText] = useState("jj");
+    const [txt , setText] = useState("");
 
     function handleChange(e){
         setText(e.target.value);
